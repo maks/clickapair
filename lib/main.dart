256977f568ae077bca71bcbaa93660bc79477137
Maksim Lin <maks@manichord.com>
2019-04-07 17:37:26 +1000

    strip spacing, use build func for rows

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,14 @@ void main() => runApp(App());
 
 const appName = 'Click-A-Pair';
 final rng = math.Random();
-
 List<String> images = [
-"❤️️","🙂","🌻","🏝","⚡️","☘","☀️","🌏","🚀","🚲","🚢","🚦","🚗","🎁","🏀️","⚽️","🏆","🏎","🃏","🏁","🇦🇺","💯","🔔","📡","📺","⏰","✏️","💎","🕶","💾","🔔","🔑","💡","🏊‍","🦄","️🎲","🎱","🏠","🏛","🐱","🐧","🐦","🐔","🐞","🦀","📱","🚜","⚓","️🍍","🍏","🍓","🍒","🍭","🌶","🕰","🎻","🎨",
-];
+"❤️️","🙂","🌻","🏝","⚡️","☘","☀️","🌏","🚀","🚲","🚢","🚦","🚗","🎁","🏀️","⚽️","🏆","🏎","🃏","🏁","🇦🇺","💯","🔔","📡","📺","⏰","✏️","💎","🕶","💾","🔔","🔑","💡","🏊‍","🦄","️🎲","🎱","🏠","🏛","🐱","🐧","🐦","🐔","🐞","🦀","📱","🚜","⚓","️🍍","🍏","🍓","🍒","🍭","🌶","🕰","🎻","🎨",];
 
 class App extends StatelessWidget {
   App({Key key}) : super(key: key);
-
-  Future<String> _loadCards(BuildContext context) async {
+  Future<String> _load(BuildContext context) async {
     return await DefaultAssetBundle.of(context).loadString('assets/cards.json');
   }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,7 +24,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: FutureBuilder<Object>(
-          future: _loadCards(context),
+          future: _load(context),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               final d = List<dynamic>.from(json.decode(snapshot.data));
@@ -44,72 +40,52 @@ class App extends StatelessWidget {
     );
   }
 }
-
 class GameStateProvider extends InheritedWidget {
   final _GameBoardState gameState;
-
   GameStateProvider({Key key, child, this.gameState})
       : super(key: key, child: child);
-
   Map<String, int> get score => gameState.score;
-
   void updateScore(String playerName) {
     gameState.setState(() {
       gameState.score[playerName]++;
     });
   }
-
   bool isMatchingRoundItem(int index) => index == gameState.roundMatchItem;
-
   static GameStateProvider of(BuildContext context) {
     return (context.inheritFromWidgetOfExactType(GameStateProvider)
         as GameStateProvider);
   }
-
   @override
   bool updateShouldNotify(GameStateProvider oldWidget) => true;
 }
 
 class GameBoard extends StatefulWidget {
   final List<List<int>> cards;
-
   GameBoard({
     Key key,
     this.cards,
   }) : super(key: key);
-
   @override
   _GameBoardState createState() => _GameBoardState();
 }
 
 class _GameBoardState extends State<GameBoard> {
   int roundMatchItem;
-  final Map<String, int> score = {
-    "Player 1": 0,
-    "Player 2": 0,
-  };
-
-  List<int> _randomCard(BuildContext context) {
-    return widget.cards.elementAt(rng.nextInt(55))?.map((c) => c)?.toList() ??
-        [];
+  final Map<String, int> score = {"Player 1": 0,"Player 2": 0,};
+  List<int> _rndCard(BuildContext context) {
+    return widget.cards.elementAt(rng.nextInt(55))?.map((c) => c)?.toList() ?? [];
   }
-
   int _roundItem(List<int> a, List<int> b) {
     return a.firstWhere((x) => b.contains(x));
   }
-
   @override
   Widget build(BuildContext context) {
-    final itemsA = _randomCard(context);
-    final itemsB = _randomCard(context);
+    final itemsA = _rndCard(context);
+    final itemsB = _rndCard(context);
     roundMatchItem = _roundItem(itemsA, itemsB);
-
     return GameStateProvider(
       gameState: this,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(appName),
-        ),
+      child: Scaffold(appBar: AppBar(title: Text(appName),),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
@@ -117,13 +93,8 @@ class _GameBoardState extends State<GameBoard> {
               cardName: score.keys.first,
               items: itemsA,
             ),
-            Divider(
-              color: Colors.black,
-            ),
-            GameCard(
-              cardName: score.keys.last,
-              items: itemsB,
-            ),
+            Divider(color: Colors.black,),
+            GameCard(cardName: score.keys.last,items: itemsB,),
           ],
         ),
       ),
@@ -134,12 +105,9 @@ class _GameBoardState extends State<GameBoard> {
 class GameCard extends StatelessWidget {
   final String cardName;
   final List<int> items;
+  const GameCard({Key key,this.cardName,this.items,}) : super(key: key);
 
-  const GameCard({
-    Key key,
-    this.cardName,
-    this.items,
-  }) : super(key: key);
+  _getRow(name, items, start, end) => CardRow(cardName:name,rowItems:items.sublist(start, end));
 
   @override
   Widget build(BuildContext context) {
@@ -153,18 +121,9 @@ class GameCard extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    CardRow(
-                      cardName: cardName,
-                      rowItems: items.sublist(0, 3),
-                    ),
-                    CardRow(
-                      cardName: cardName,
-                      rowItems: items.sublist(3, 6),
-                    ),
-                    CardRow(
-                      cardName: cardName,
-                      rowItems: items.sublist(6, 8),
-                    ),
+                    _getRow(cardName, items, 0, 3),
+                    _getRow(cardName, items, 3, 6),
+                    _getRow(cardName, items, 6, 8),
                   ],
                 ),
               ),
@@ -182,9 +141,7 @@ class GameCard extends StatelessWidget {
 
 class CardScore extends StatelessWidget {
   final String name;
-
   CardScore({Key key, this.name}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -202,9 +159,7 @@ class CardScore extends StatelessWidget {
 class CardRow extends StatelessWidget {
   final String cardName;
   final List<int> rowItems;
-
   CardRow({Key key, this.cardName, this.rowItems}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -235,17 +190,13 @@ class CardRow extends StatelessWidget {
 class CardItem extends StatelessWidget {
   final int index;
   final String name;
-
   const CardItem({Key key, this.index, this.name}) : super(key: key);
-
   double _randAngle() {
     return rng.nextInt(360).toDouble();
   }
-
   double _randomFontSize() {
     return ((rng.nextInt(4) * 9) + 22).toDouble();
   }
-
   @override
   Widget build(BuildContext context) {
     return Transform.rotate(
